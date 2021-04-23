70cbeeb2300512d96f10f3ebfae47cfb7027eaea
milkon <melkon_2010@hotmail.com>
2021-04-23 17:44:01 +0200

    Useing geolocations and Leaflet to get a map an mnipulate it

--- script.js
+++ script.js
@@ -22,3 +22,47 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const longitude = position.coords.longitude;
+      console.log(`https://www.google.se/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13); //coords and map zoom
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+    },
+
+    function () {
+      alert('Could not get your position');
+    }
+  );
